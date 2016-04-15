d57a14e1c898df4f08fc57363029079cae471a6b
heimann <heimann@172.29.10.121>
2016-04-15 14:12:50 +0200

    xml test

--- src/main/scala/configMgr/ConfigFile.scala
+++ src/main/scala/configMgr/ConfigFile.scala
@@ -1,14 +1,18 @@
 package configMgr
 
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
+//import org.json4s._
+//import org.json4s.jackson.JsonMethods._
 
 class ConfigFile {
   def getJson : Unit = {
-    val json_string = scala.io.Source.fromFile("src/main/scala/configMgr/config.json").getLines.mkString
+//    val json_string = scala.io.Source.fromFile("src/main/scala/configMgr/config.json").getLines.mkString
 //    println(json_string)
-    val jsonObject = parse("" + json_string + "")
-    val steps = jsonObject \ "steps"
+//    val jsonObject = parse("" + json_string + "")
+//    val steps = jsonObject \ "steps"
+//    println(steps)
+    
+    val xmlTest = scala.xml.XML.loadFile("src/main/scala/configMgr/config.xml")
+    println(xmlTest)
 //    println(jsonObject)
   }
 }
