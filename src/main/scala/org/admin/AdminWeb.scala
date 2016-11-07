d13f1e73d34e8887e7cd70101d1bc3e856339029
heimann <heimann@172.29.10.121>
2016-11-07 18:26:04 +0100

    define Json messages for communicate between Client and Server

--- src/main/scala/org/admin/AdminWeb.scala
+++ src/main/scala/org/admin/AdminWeb.scala
@@ -10,24 +10,55 @@ trait AdminWeb {
   
   
   /**
-   * 001 => register Admin 
-   * 002 => authenticate Admin
-   * 003 => configTree
-   * 004 => addStep
-   * 005 => addComponent and connect Step with Component
-   * 006 => add NextStep add connect Component with NextStep
-   * 
+   * 1. register => register Admin
+   *   Server <- Client
+   *   {"jsonId": 1, "method": "register", "params": {"username": "test", "password": "test"}}
+   *   Server -> Client
+   *   {"jsonId": 1, "method": "register", result": {"adminId": "AU#40:0", "username": "test"}}
+   * 2. => authenticate Admin
+   *   Server <- Client
+   *   {"jsonId": 2, "method": "autheticate", params: {"username": "test", "password": "test"}}
+   *   Server -> Client
+   *   {"jsonId": 2, "method": "autheticate", result: {"authentication": true, "username": "test", "password": "test"}}
+   * 3. => configTree
+   *   Server <- Client
+   *   {"jsonId": 3, "method": "configTree", params: {}}
+   *   Server -> Client
+   *   {"jsonId": 3, "method": "configTree", result: { TODO definieren}}
+   * 4. => addFirstStep
+   *   Server <- Client
+   *   {"jsonId": 4, "method": "addFirstStep", "params": {"adminId": "AU#40:0", "kind": "immutable"}}
+   *   Server -> Client
+   *   {"jsonId": 4, "method": "addFirstStep", 
+   *     "result": {"id": "#12.1", "stepId": "S#12.1", "adminId": "AU#40:0", "kind": "first" }} 
+   * 5. => addComponent and connect Step with Component
+   *   Server <- Client
+   *   {"jsonId": 5, "method": "addComponent", "params": {"adminId": "AU#40:0", "kind": "immutable", "stepId": "#12:1"}
+   *    Server -> Client
+   *    {"jsonId": 5, "method": "addComponent", 
+   *      "result": {"id": "#13:1", "componentId": "C#13:1", "adminId": "AU#40:0", "kind": "immutable"}}
+   * 6. => addNextStep add connect Component with NextStep
+   *   Server <- Client
+   *   {"jsonId": 6, "method": "addNextStep", "params": {"adminId": "AU#40:0", "kind": "default", "componentId": "#13:1"}
+   *   Server -> Client
+   *    {"jsonId": 6, "method": "addNextStep", 
+   *      "result": {"id": "#14:1", "stepId": "S#14:1", "adminId": "AU#40:0", "kind": "default"}}
+   * 7. => updateStep update Step
+   * 8. => deleteStep delete Step and its hasComponent  (brauche zugehörige Step, was soll mit der weiterer ConfigTree passieren)
+   * 9. => updateComponent 
+   * 10. => deleteComponen
    */
   
   def handelMessage(receivedMessage: JsValue): JsValue = {
     (receivedMessage \ "method").toString() match {
-      case "004" => addStep(receivedMessage)
-      case "002" => addComponent(receivedMessage)
+      case "addFirstStep" => addFirstStep(receivedMessage)
+      case "addComponent" => addComponent(receivedMessage)
+      case "addNextStep" => addNextStep(receivedMessage)
     }
   }
   
   
-  private def addStep(receivedMessage: JsValue): JsValue = {
+  private def addFirstStep(receivedMessage: JsValue): JsValue = {
     //TODO impl Reads with validation show 
     // https://www.playframework.com/documentation/2.4.x/ScalaJsonCombinators
     val adminId = (receivedMessage \ "adminId").toString()
@@ -43,6 +74,15 @@ trait AdminWeb {
     val component = Admin.addComponent(new AdminComponent("", "", adminId, kind))
     val hasComponent = Admin.addHasComponent(adminId, stepId, component.id)
     Json.toJson(component)
+  }
+  
+  private def addNextStep(receivedMessage: JsValue): JsValue = {
+    val adminId = (receivedMessage \ "adminId").toString()
+    val kind = (receivedMessage \ "kind").toString()
+    val componentId = (receivedMessage \ "componentId").toString()
+    val step = Admin.addStep(new AdminStep("", "", adminId, kind))
+    val nextStep = Admin.addNextStep(adminId, componentId, step.id)
+    Json.toJson(step)
   }
   
   implicit val adminStepWrites = new Writes[AdminStep] {
