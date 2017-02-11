f305bfd8beccc4ec09aa2032495ef73767817f91
Gennadi Heimann <genaheimann@gmail.com>
2017-02-11 23:57:16 +0100

    db connection over scala code

---
+++ src/main/scala/org/persistence/GlobalConfigForDB.scala
@@ -0,0 +1,23 @@
+package org.persistence
+
+object GlobalConfigForDB {
+  val activeDB: String = "test"
+  //Test
+  def db: DB = {
+    activeDB match {
+      case "test" => new TestDB
+    }
+  }
+}
+
+abstract class DB {
+  val dbName: String
+  val username: String
+  val password: String
+}
+
+class TestDB extends DB {
+  override val dbName = "testDB"
+  override val username = "root"
+  override val password = "root"
+}
--- src/main/scala/org/persistence/db/orientdb/OrientDB.scala
+++ src/main/scala/org/persistence/db/orientdb/OrientDB.scala
@@ -3,37 +3,23 @@ package org.persistence.db.orientdb
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory
 import com.tinkerpop.blueprints.impls.orient.OrientGraph
 import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal
+import org.persistence.GlobalConfigForDB
 
 object OrientDB {
   
-  
+  /**
+   * @author Gennadi Heimann
+   * 
+   * @version 1.0
+   * 
+   * @param
+   * 
+   * @return
+   */
   def getGraph(): OrientGraph = {
     
-    val xmlDBConfig = scala.xml.XML.loadFile("global_config/globalConfig.xml")
-    
-    val activeDB: String = (xmlDBConfig \"activeDB" \ "dbName").text
-    val dbs : scala.xml.NodeSeq = (xmlDBConfig \ "db")
-    
-    val db: List[DB] = dbs.filter (k => (k \ "dbName").text == activeDB ).map(db =>{
-      new DB((db \ "kind").text, (db \ "dbName").text, (db \ "username").text, (db \ "password").text)
-    }).toList
-    
-    val uri = if(System.getProperty("os.name") == "Linux") 
-                "remote:localhost/" + db(0).dbName
-              else 
-                "remote:localhost/" + db(0).dbName
-    
-    val factory:  OrientGraphFactory = new OrientGraphFactory(uri, db(0).username, db(0).password)
-    factory.getTx()
-  }
-  
-  def getGraph(db: String, username: String, password: String) = {
-    val uri = if(System.getProperty("os.name") == "Linux") 
-                "remote:localhost/" + db 
-              else 
-                "remote:localhost/" + db
-    
-    val factory:  OrientGraphFactory = new OrientGraphFactory(uri, username, password)
+    val uri = "remote:localhost/" + GlobalConfigForDB.db.dbName
+    val factory:  OrientGraphFactory = new OrientGraphFactory(uri, GlobalConfigForDB.db.username, GlobalConfigForDB.db.password)
     factory.getTx()
   }
 }
