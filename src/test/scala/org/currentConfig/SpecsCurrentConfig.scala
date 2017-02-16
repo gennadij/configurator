654d91c48c32b6f9d2187b5c5211ac5f32e87801
Gennadi Heimann <genaheimann@gmail.com>
2017-02-16 23:43:39 +0100

    SpecsCurrentConfig auf der 2. Step erweitert

--- src/test/scala/org/currentConfig/SpecsCurrentConfig.scala
+++ src/test/scala/org/currentConfig/SpecsCurrentConfig.scala
@@ -22,27 +22,65 @@ class SpecsCurrentConfig extends Specification with BeforeAfterAll with Config{
   def afterAll(): Unit = {}
   def beforeAll(): Unit = {}
   
-  "Diese Specifikation spezifiziert die aktuelle KOnfiguration" >> {
-    val startConfigCS = Json.obj(
-        "dtoId" -> DTOIds.startConfig,
-        "dto" -> DTONames.startConfig,
-        "params" -> Json.obj(
-            "configUrl" -> "http://contig/user10"
+  "Diese Specifikation spezifiziert die aktuelle Konfiguration" >> {
+    "StartConfig" >> {
+      val startConfigCS = Json.obj(
+          "dtoId" -> DTOIds.startConfig,
+          "dto" -> DTONames.startConfig,
+          "params" -> Json.obj(
+              "configUrl" -> "http://contig/user10"
+          )
+      )
+      val startConfigSC: JsValue = handleMessage(startConfigCS)
+      
+      println(startConfigSC)
+      
+      "result \\ currentConfig.size" >> {
+          (startConfigSC \ "result" \ "currentConfig" ).asOpt[Seq[JsValue]].get.size === 1
+      }
+      "result \\ currentConfig(0) \\ nameToShow" >> {
+        (((startConfigSC \ "result" \ "currentConfig")(0)) \ "nameToShow").asOpt[String].get === "First Step"
+      }
+      "result \\ currentConfig(0) \\ components.size" >> {
+        (((startConfigSC \ "result" \ "currentConfig")(0)) \ "components").asOpt[Seq[JsValue]].get.size === 0
+      }
+      val selectedComponent = (((startConfigSC \ "result" \ "step" \ "components")(0)) \ "componentId").asOpt[String].get
+      "2. Step -> selectedComponent " + selectedComponent >> {
+        val nextStepCS = Json.obj(
+            "dtoId" -> DTOIds.nextStep,
+            "dto" -> DTONames.nextSTep,
+            "params" -> Json.obj(
+                "componentIds" -> Json.arr(selectedComponent)
+            )
         )
-    )
-    val startConfigSC: JsValue = handleMessage(startConfigCS)
-    
-    println(startConfigSC)
-    
-    "result \\ currentConfig.size" >> {
-        (startConfigSC \ "result" \ "currentConfig" ).asOpt[Seq[JsValue]].get.size === 1
-    }
-    "result \\ currentConfig(0) \\ nameToShow" >> {
-      (((startConfigSC \ "result" \ "currentConfig")(0)) \ "nameToShow").asOpt[String].get === "First Step"
-    }
-    "result \\ currentConfig(0) \\ components.size" >> {
-      (((startConfigSC \ "result" \ "currentConfig")(0)) \ "components").asOpt[Seq[JsValue]].get.size === 0
+        println(nextStepCS)
+        val nextStepSC: JsValue = handleMessage(nextStepCS)
+        println(nextStepSC)
+        "result \\ currentConfig.size" >> {
+          (startConfigSC \ "result" \ "currentConfig" ).asOpt[Seq[JsValue]].get.size === 2
+        }
+        "result \\ currentConfig(0) \\ nameToShow" >> {
+          (((startConfigSC \ "result" \ "currentConfig")(0)) \ "nameToShow").asOpt[String].get === "First Step"
+        }
+        "result \\ currentConfig(0) \\ components.size" >> {
+          (((startConfigSC \ "result" \ "currentConfig")(0)) \ "components").asOpt[Seq[JsValue]].get.size === 1
+        }
+        "result \\ currentConfig(0) \\ components(0) \\ componentId" >> {
+          ((((startConfigSC \ "result" \ "currentConfig")(0)) \ "components")(0) \ "componentId").asOpt[String].get === 
+            selectedComponent
+        }
+        "result \\ currentConfig(0) \\ components(0) \\ nameToShow" >> {
+          ((((startConfigSC \ "result" \ "currentConfig")(0)) \ "components")(0) \ "nameToShow").asOpt[String].get === 
+            "Component"
+        }
+        "result \\ currentConfig(1) \\ nameToShow" >> {
+          (((startConfigSC \ "result" \ "currentConfig")(1)) \ "nameToShow").asOpt[String].get === "First Step"
+        }
+        "result \\ currentConfig(1) \\ components.size" >> {
+          (((startConfigSC \ "result" \ "currentConfig")(1)) \ "components").asOpt[Seq[JsValue]].get.size === 0
+        }
+        
+      }
     }
   }
-  
 }
