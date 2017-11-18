dacab26d91f6cda997b22a06831f342a1e95ff8e
heimann <heimann@172.29.10.121>
2017-11-18 10:39:24 +0100

    Impl Currentconfig nur fuer zum Hinzufuegen von komponenten

--- app/models/currentConfig/CurrentConfig.scala
+++ app/models/currentConfig/CurrentConfig.scala
@@ -17,22 +17,9 @@ import play.api.Logger
  * Crated by Gennadi Heimann 28.02.2017
  */
 object CurrentConfig {
-  /**
-   * @author Gennadi Heimann
-   * 
-   * @version 0.0.1
-   * 
-   * Initialisierung des CurentConfig
-   * Es wird nur erster Schritt ohne seine Komponente hinzugefuegt
-   * 
-   * @param FirstStep
-   * 
-   * @return
-   */
-  def init(firstStep: Step) = {
-    
-  }
   
+  
+  val currentConfig: CurrentConfig = new CurrentConfig
   /**
    * @author Gennadi Heimann
    * 
@@ -40,15 +27,15 @@ object CurrentConfig {
    * 
    * Neu ausgewaelte Komponente wird zu seinem Schritt eingefuegt
    * 
-   * @param Step
+   * @param 
    * 
    * @return
    */
-  def addComponent = {
+  def addComponent(step: StepCurrentConfig, component: Component): Unit = {
     // es wird geprueft ob der Step schon angelegt war
     // Die erste Komponente in dem Schritt wird ohne Pruefung des SelectionCriterium hinzugefuegt
     // Die Abhaengigkeiten werden weiterhin jedes mal geprueft
-    ???
+    currentConfig.addComponent(step, component)
   }
   
   /**
@@ -56,17 +43,14 @@ object CurrentConfig {
    * 
    * @version 0.0.1
    * 
-   * Wenn alle Komponente des Schrittes ausgewaelt und bearbeitet waren,
-   * wird ein neuen Schritt geladen.
-   * 
    * Dieser neuer Schritt wird in die CurrentConfig hinzugefuegt
    * 
-   * @param Step
+   * @param 
    * 
    * @return
    */
-  def addNextStep = {
-    ???
+  def addStep(nextStep: Option[StepCurrentConfig], fatherStep: Option[StepCurrentConfig]): Unit = {
+    currentConfig.addStep(nextStep, fatherStep)
   }
   
   /**
@@ -78,68 +62,7 @@ object CurrentConfig {
    * 
    * @return CurrentConfig
    */
-  def getCurrentConfig = ???
-  
-  
-  private val currentConfigs: Map[String, List[Step]] = Map.empty
-  
-  val currentConfig: CurrentConfig = new CurrentConfig
-  
-  /**
-   * @author Gennadi Heimann
-   * 
-   * @version 0.0.1
-   * 
-   * @param String, Step
-   * 
-   * @return Unit
-   */
-  def setCurrentConfig(step: Step): Unit = {
-    
-    
-    
-    
-    
-    
-    
-    
-//    val currentConfigSteps: List[Step] = currentConfigs.get(clientId) match {
-//      case Some(step) => currentConfigs.get(clientId).get
-//      case None => currentConfig.setCurrentConfig(step, List[Step]())
-////      case _ => println("Fehler bei CurrentConfig")
-//    }
-//    
-//    val indexOfSelectedStep = currentConfigSteps.indexWhere(s => s.stepId == step.stepId)
-//    
-//    if(indexOfSelectedStep == -1) {
-//      currentConfigs += (clientId -> (currentConfigSteps :+ step))
-//    }else {
-//       val countOfStepToDelete = currentConfigSteps.size - indexOfSelectedStep
-//       val steps = currentConfigSteps.dropRight(countOfStepToDelete)
-//       currentConfigs. += (clientId -> (steps :+ step))
-//    }
-  }
-  
-  /**
-   * @author Gennadi Heimann
-   * 
-   * @version 0.0.1
-   * 
-   * @param CurrentConfigIn
-   * 
-   * @return CurrentConfigOut
-   */
-  def getCurrentConfig(currentConfigIn: CurrentConfigIn): CurrentConfigOut = {
-    
-    val currentConfigSteps: List[Step] = currentConfigs.get(currentConfigIn.clientId) match {
-      case Some(step) => step
-      case None => List[Step]()
-    }
-    
-    CurrentConfigOut(
-        currentConfigSteps
-    )
-  }
+  def getCurrentConfig = currentConfig.getCurrentConfig
   
   /**
    * @author Gennadi Heimann
@@ -148,10 +71,10 @@ object CurrentConfig {
    * 
    * @param 
    * 
-   * @return Map[String, List[Step]]
+   * @return Unit
    */
-  def getCuttentConfig: Map[String, List[Step]] = {
-    currentConfigs
+  def printCurrentConfig: Unit = {
+    currentConfig.printCurrentConfig
   }
 }
 
@@ -250,7 +173,7 @@ class CurrentConfig {
    * 
    * @return Unit
    */
-  def printCurrentConfig: Unit = {
+  private def printCurrentConfig: Unit = {
     Logger.info("Current Configuration")
     getNextStep(this.firstStep)
   }
